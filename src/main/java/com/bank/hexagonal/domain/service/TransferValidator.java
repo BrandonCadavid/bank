059fd0e325e371b4dde8505659ba54b9f9a98ee6
Brandon Cadavid <brandoncadavid1@gmail.com>
2026-03-17 06:12:14 -0500

    changes infrasctructure

--- src/main/java/com/bank/hexagonal/domain/service/TransferValidator.java
+++ src/main/java/com/bank/hexagonal/domain/service/TransferValidator.java
@@ -1,6 +1,8 @@
 package com.bank.hexagonal.domain.service;
 
 import com.bank.hexagonal.domain.model.Account;
+import lombok.experimental.UtilityClass;
+
 import java.math.BigDecimal;
 
 /**
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
  * Se usa cuando una regla de negocio involucra múltiples entidades
  * o no tiene sentido dentro de una clase Account.
  */
+@UtilityClass
 public class TransferValidator {
     public static void validate(Account from, BigDecimal amount) {
         if (amount.compareTo(new BigDecimal("10000")) > 0) {
